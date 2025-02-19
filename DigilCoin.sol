ea367381c1a8c2812852965e71f10a81876d22b4
Himawari G <github@gsolo.com>
2025-02-19 05:21:51 -0500

    Update Digil Coin OpenZeppelin 5+

--- DigilCoin.sol
+++ DigilCoin.sol
@@ -1,14 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-/// @custom:security-contact security@digil.co.in
-contract DigilCoin is ERC20, Pausable, Ownable {
-    constructor() ERC20("Digil Coin", "DIGIL") {
-        _mint(msg.sender, 7926886297 * 10 ** decimals());
+contract DigilCoin is ERC20, ERC20Pausable, Ownable, ERC20Permit {
+    constructor(address initialOwner) ERC20("Digil Coin", "DIGIL") Ownable(initialOwner) ERC20Permit("Digil Coin")
+    {
+        _mint(initialOwner, 8208008028 * 10 ** decimals());
     }
 
     function pause() public onlyOwner {
@@ -23,11 +24,8 @@ contract DigilCoin is ERC20, Pausable, Ownable {
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable)
     {
-        super._beforeTokenTransfer(from, to, amount);
+        super._update(from, to, value);
     }
 }
