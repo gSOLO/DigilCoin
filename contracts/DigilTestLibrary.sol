d0ee699369535cd86ecd65d87b1b925aef8c749f
Himawari G <github@gsolo.com>
2025-08-08 04:24:16 -0400

    Update DigilTestLibrary.sol

--- contracts/DigilTestLibrary.sol
+++ contracts/DigilTestLibrary.sol
@@ -8,6 +8,6 @@ library DigilTestLibrary {
         return IERC20(0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99);
     }
     function getToken() public pure returns (IDigilToken) {
-        return IDigilToken(0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692);
+        return IDigilToken(0xAAC129A3e6e9f44147951dDD5655d66c312A4713);
     }
 }
