3a1fd133be7d78b8d67c27043a80029962faf9c7
Himawari G <github@gsolo.com>
2025-11-18 09:03:53 -0500

    Update Tests

--- tests/DigilTokenB_test.sol
+++ tests/DigilTokenB_test.sol
@@ -39,7 +39,7 @@ contract BetaTestSuite {
         uint256 tokenId = digil.createToken(1000000000000000, 1000000000000000000, false, 4, "Test Withdraw");
 
         (uint256 withdrawlCoins, uint256 withdrawlValue) = digil.withdraw();
-        Assert.equal(withdrawlCoins, 100 * 10 ** 18, "First withdrawl should be 100 coins");
+        Assert.equal(withdrawlCoins, 5000 * 10 ** 18, "First withdrawl should be 5000 coins");
         Assert.equal(withdrawlValue, 0, "First withdrawl should be 0 value");
 
         // Approve the Digil Token contract to spend the specified coinAmount.
