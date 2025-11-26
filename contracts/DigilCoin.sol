7ce90d1c2c8ef2f239a986fb7220c3fd99880908
Himawari G <github@gsolo.com>
2025-11-26 13:10:18 -0500

    Implement ERC20Viotes

--- contracts/DigilCoin.sol
+++ contracts/DigilCoin.sol
@@ -6,19 +6,22 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
 /// @title Digil Coin (ERC20)
 /// @author gSOLO
 /// @notice ERC20 contract used to facilitate actions with the Digil Token
 /// @custom:security-contact security@digil.co.in
-contract DigilCoin is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20Permit {
+contract DigilCoin is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20Permit, ERC20Votes {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor(address initialOwner) ERC20("Digil Coin", "DIGIL") ERC20Permit("Digil Coin") {
-        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
-        _grantRole(PAUSER_ROLE, initialOwner);
-        _grantRole(MINTER_ROLE, initialOwner);
+    constructor(address defaultAdmin) ERC20("Digil Coin", "DIGIL") ERC20Permit("Digil Coin")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(PAUSER_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, defaultAdmin);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -33,7 +36,22 @@ contract DigilCoin is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20P
         _mint(to, amount);
     }
 
-    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
+    function clock() public view override returns (uint48) {
+        return uint48(block.timestamp);
+    }
+
+    // solhint-disable-next-line func-name-mixedcase
+    function CLOCK_MODE() public pure override returns (string memory) {
+        return "mode=timestamp";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable, ERC20Votes) {
         super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 }
