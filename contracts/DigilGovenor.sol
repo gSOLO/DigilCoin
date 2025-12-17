3d6e8c85a9dc95f59b0d6516feb00607bd8378da
Himawari G <github@gsolo.com>
2025-12-16 21:39:04 -0500

    Create Digil Timelock

--- contracts/DigilGovenor.sol
+++ contracts/DigilGovenor.sol
@@ -10,83 +10,64 @@ import {GovernorVotes} from "@openzeppelin/contracts/governance/extensions/Gover
 import {GovernorVotesQuorumFraction} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
+/// @title Digil Governor
+/// @author gSOLO
 /// @custom:security-contact security@digil.co.in
-contract DigilGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorStorage, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
-    constructor(IVotes _token, TimelockController _timelock)
-        Governor("Digil Governor")
-        GovernorSettings(1 days, 1 weeks, 10000e18)
-        GovernorVotes(_token)
-        GovernorVotesQuorumFraction(5)
-        GovernorTimelockControl(_timelock)
-    {}
+contract DigilGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorStorage, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl, AccessControl {
+    bytes32 public constant VETO_ROLE = keccak256("VETO_ROLE");
+
+    constructor(address defaultAdmin, IVotes _token, TimelockController _timelock) Governor("Digil Governor") GovernorSettings(1 days, 1 weeks, 10000e18) GovernorVotes(_token) GovernorVotesQuorumFraction(5) GovernorTimelockControl(_timelock) {
+        // Grant the specified admin the ability to Veto
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(VETO_ROLE, defaultAdmin);
+    }
+
+    /**
+     * @notice Allows an account with VETO_ROLE to cancel any proposal.
+     * @dev Calls the internal _cancel function.
+     */
+    function veto(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash) public onlyRole(VETO_ROLE) {
+        // _cancel returns the proposalId, we can ignore it here
+        _cancel(targets, values, calldatas, descriptionHash);
+    }
 
     // The following functions are overrides required by Solidity.
 
-    function state(uint256 proposalId)
-        public
-        view
-        override(Governor, GovernorTimelockControl)
-        returns (ProposalState)
-    {
+    function state(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (ProposalState) {
         return super.state(proposalId);
     }
 
-    function proposalNeedsQueuing(uint256 proposalId)
-        public
-        view
-        override(Governor, GovernorTimelockControl)
-        returns (bool)
-    {
+    function proposalNeedsQueuing(uint256 proposalId) public view override(Governor, GovernorTimelockControl) returns (bool) {
         return super.proposalNeedsQueuing(proposalId);
     }
 
-    function proposalThreshold()
-        public
-        view
-        override(Governor, GovernorSettings)
-        returns (uint256)
-    {
+    function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
         return super.proposalThreshold();
     }
 
-    function _propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description, address proposer)
-        internal
-        override(Governor, GovernorStorage)
-        returns (uint256)
-    {
+    function _propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description, address proposer) internal override(Governor, GovernorStorage) returns (uint256) {
         return super._propose(targets, values, calldatas, description, proposer);
     }
 
-    function _queueOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-        internal
-        override(Governor, GovernorTimelockControl)
-        returns (uint48)
-    {
+    function _queueOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash) internal override(Governor, GovernorTimelockControl) returns (uint48) {
         return super._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
     }
 
-    function _executeOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-        internal
-        override(Governor, GovernorTimelockControl)
-    {
+    function _executeOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash) internal override(Governor, GovernorTimelockControl) {
         super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
     }
 
-    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-        internal
-        override(Governor, GovernorTimelockControl)
-        returns (uint256)
-    {
+    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash) internal override(Governor, GovernorTimelockControl) returns (uint256) {
         return super._cancel(targets, values, calldatas, descriptionHash);
     }
 
-    function _executor()
-        internal
-        view
-        override(Governor, GovernorTimelockControl)
-        returns (address)
-    {
+    function _executor() internal view override(Governor, GovernorTimelockControl) returns (address) {
         return super._executor();
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(Governor, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
---
+++ contracts/DigilTimelock.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.31;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+/// @title Digil Timelock Controller
+/// @author gSOLO
+/// @notice This contract holds the funds and the "Owner" permissions for the DAO.
+/// @custom:security-contact security@digil.co.in
+contract DigilTimelock is TimelockController {
+    /**
+     * @dev Constructor.
+     * @param minDelay The minimum time (in seconds) a proposal must wait before execution.
+     * @param proposers List of addresses allowed to propose (usually just the Governor).
+     * @param executors List of addresses allowed to execute (usually the Governor or address(0)).
+     * @param admin The address that can grant/revoke roles (initially you, later the Timelock itself).
+     */
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors, address admin) TimelockController(minDelay, proposers, executors, admin) {
+
+    }
+}
