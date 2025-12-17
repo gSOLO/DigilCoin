207debe14757efcf3c464a2f4427e263d352cbb6
Himawari G <github@gsolo.com>
2025-12-17 07:32:23 -0500

    Update ERC20 Interface Licenses

--- contracts/IERC20Burnable.sol
+++ contracts/IERC20Burnable.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.31;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// Interface to allow calling the mint function on the ERC20 token
+// Interface to allow calling the burn function on the ERC20 token
 interface IERC20Burnable is IERC20 {
     function burn(uint256 amount) external;
     function burnFrom(address account, uint256 amount) external;
